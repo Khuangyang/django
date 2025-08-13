919942ece7d7654692e295a933f9728392bbdd12
Khuangyang <153403523+Khuangyang@users.noreply.github.com>
2025-08-13 07:10:55 +0000

    fixing_publish_error

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -44,8 +45,8 @@ def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    if request.method=='POST':
-        post.publish()
+    post.publish()
     return redirect('post_detail', pk=pk)
