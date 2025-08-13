f9f85836ffaca4571235eef7c1d3f45e209d13f5
Khuangyang <153403523+Khuangyang@users.noreply.github.com>
2025-08-13 03:56:57 +0000

    views_import_redirect

--- djangogirls/blog/views.py
+++ djangogirls/blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
