fbb74baab91f93d83d89354431fb22308d12cb32
Khuangyang <153403523+Khuangyang@users.noreply.github.com>
2025-08-14 01:32:15 +0000

    update_url_logout

--- djangogirls/mysite/urls.py
+++ djangogirls/mysite/urls.py
@@ -18,10 +18,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
+from blog import views as blog_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout')
+    path('accounts/logout/', blog_views.custom_logout, name='logout')
 ]
