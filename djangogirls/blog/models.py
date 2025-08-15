2ed72f6a0e9c1ebc8e30e438bf990e19e212f0d5
Khuangyang <153403523+Khuangyang@users.noreply.github.com>
2025-08-15 01:45:55 +0000

    models_HomePage

--- djangogirls/blog/models.py
+++ djangogirls/blog/models.py
@@ -32,3 +32,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+class HomePage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
